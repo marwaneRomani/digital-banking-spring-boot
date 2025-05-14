69e230e7cf55e905c615209360f083e7b0722973
marwane <marwanromani2018@gmail.com>
2025-05-14 17:33:27 +0100

    add agent repository

---
+++ src/main/java/com/marwane/server/repositories/AgentRepository.java
@@ -0,0 +1,13 @@
+package com.marwane.server.repositories;
+
+import com.marwane.server.models.users.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AgentRepository extends JpaRepository<Agent, Long> {
+    Optional<Agent> findByEmail(String email);
+
+}
