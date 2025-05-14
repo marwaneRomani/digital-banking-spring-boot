599e380e55f8c1611405122a83e1189aa646d171
marwane <marwanromani2018@gmail.com>
2025-05-14 16:55:15 +0100

    add jwt utilities class

---
+++ src/main/java/com/marwane/server/utils/JwtUtil.java
@@ -0,0 +1,105 @@
+package com.marwane.server.utils;
+
+import com.marwane.server.models.users.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtil {
+    String secretKey = "";
+
+    public String generateToken(UserDetailsImpl userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>() {{
+            put("type", "access");
+            put("email", userDetails.getUsername());
+            put("roles", userDetails.getAuthorities());
+        }};
+
+        return buildToken(extraClaims, userDetails, 1000L * 60 * 60);
+    }
+
+    // Validate JWT token
+    public boolean validateToken(String token, String username) {
+        return username.equals(getUsernameFromToken(token)) && !isTokenExpired(token);
+    }
+
+
+    public String extractUserName(String token) {
+        return (String) extractAllClaims(token).get("email");
+    }
+
+    Claims extractAllClaims(String token) throws MalformedJwtException {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Extract username from JWT token
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // Check if the token is expired
+    private boolean isTokenExpired(String token) {
+        return getExpirationDateFromToken(token).before(new Date());
+    }
+
+    // Extract expiration date from JWT token
+    private Date getExpirationDateFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+    public List<GrantedAuthority> getAuthoritiesFromToken(String token) {
+        Claims claims = extractAllClaims(token);
+
+        List<Map<String, String>> roles = claims.get("roles", List.class);
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.get("authority")))
+                .collect(Collectors.toList());
+    }
+
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, Long expiration) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+}
