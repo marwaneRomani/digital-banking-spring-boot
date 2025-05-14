ba14a85f4a50864dc47623eee62792da56787a5a
marwane <marwanromani2018@gmail.com>
2025-05-14 16:51:08 +0100

    initial project setup

---
+++ src/test/java/com/marwane/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marwane.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
