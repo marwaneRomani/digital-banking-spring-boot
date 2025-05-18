1bf1626cd72fce146889575c43db4a6700c36a28
marwane <marwanromani2018@gmail.com>
2025-05-18 16:56:37 +0100

    dockerize the backend

--- src/main/java/com/marwane/server/ServerApplication.java
+++ src/main/java/com/marwane/server/ServerApplication.java
@@ -13,30 +13,28 @@ public class ServerApplication  {
     }
 
 
-//    @Bean
-//    public CommandLineRunner dataLoader(com.marwane.server.service.auth.AuthenticationService authenticationService) {
-//        return args -> {
-//
-//            // Add an agent
-//            com.marwane.server.dtos.auth.register.RegisterUserDto agentDto = new com.marwane.server.dtos.auth.register.RegisterUserDto();
-//            agentDto.setEmail("agent@gmail.com");
-//            agentDto.setPassword("agent123");
-//            agentDto.setFullName("Marwane Agent");
-//            agentDto.setTelephone("0700000000");
-//
-//            authenticationService.signupAgent(agentDto);
-//
-//
-//            // Add a client
-//            com.marwane.server.dtos.auth.register.RegisterUserDto clientDto = new com.marwane.server.dtos.auth.register.RegisterUserDto();
-//            clientDto.setEmail("client@gmail.com");
-//            clientDto.setPassword("client123");
-//            clientDto.setFullName("Marwane Client");
-//            clientDto.setTelephone("0600000000");
-//            authenticationService.signup(clientDto);
-//
-//        };
-//    }
+   @Bean
+   public CommandLineRunner dataLoader(com.marwane.server.service.auth.AuthenticationService authenticationService) {
+       return args -> {
+
+           // Add an agent
+           com.marwane.server.dtos.auth.register.RegisterUserDto agentDto = new com.marwane.server.dtos.auth.register.RegisterUserDto();
+           agentDto.setEmail("agent@gmail.com");
+           agentDto.setPassword("agent123");
+           agentDto.setFullName("Marwane Agent");
+           agentDto.setTelephone("0700000000");
+
+           authenticationService.signupAgent(agentDto);
 
 
+           // Add a client
+           com.marwane.server.dtos.auth.register.RegisterUserDto clientDto = new com.marwane.server.dtos.auth.register.RegisterUserDto();
+           clientDto.setEmail("client@gmail.com");
+           clientDto.setPassword("client123");
+           clientDto.setFullName("Marwane Client");
+           clientDto.setTelephone("0600000000");
+           authenticationService.signup(clientDto);
+
+       };
+   }
 }
